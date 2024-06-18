a57b79c67b6c78d871a0c47ad5cf1643183231af
KiLiG-fcx <lmyannie60@163.com>
2024-06-18 19:37:28 +0800

    add demo nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,16 @@ import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
     const navigate = useNavigate();
+
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <button onClick={() => navigate('/')}>Home</button>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar;
