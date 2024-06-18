35284a8d4390897fc9df7447ba7a4dd71377bbfe
KiLiG-fcx <lmyannie60@163.com>
2024-06-18 19:33:35 +0800

    create initial files

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +14,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <div>
+          hhhh
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const NavBar = () => {
+    const navigate = useNavigate();
+}
+
+export default NavBar;
