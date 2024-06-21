5b136594f9af24279514269f71390b93501ecb60
KiLiG-fcx <lmyannie60@163.com>
2024-06-21 19:04:31 +0800

    add work box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import HomePage from './components/page/HomePage/HomePage';
-import MyWork from './components/page/MyWork/MyWork';
+import HomePage from './page/HomePage/HomePage';
+import MyWork from './page/MyWork/MyWork';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
---
+++ src/components/WorkBox.js
@@ -0,0 +1,11 @@
+import 'src\assets\styles\WorkBox.css';
+
+const WorkBox = () => {
+    return(
+        <div className="workbox">
+            box 1
+        </div>
+    )
+}
+
+export default WorkBox;
